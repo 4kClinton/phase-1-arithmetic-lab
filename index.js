2449dc1aa4a574adfe8a2a9e8f0c7ce129ba06ac
CLINTON <issnasty2@gmail.com>
2024-03-21 17:13:43 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+let multiply = num1*num2
+
+let random=0
+random++
+
+const num3 = 24
+const num4 = 10
+
+let mod = num3 % num4
+let max = 20
+Math.max("18","20")
